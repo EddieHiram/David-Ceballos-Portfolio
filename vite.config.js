39bd182a2f047c2ad7008343b891ffb5d232a22e
EddieHiram <ehceballosf@miners.utep.edu>
2026-05-04 00:58:24 -0600

    Update site for custom domain

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/David-Ceballos-Portfolio/',
+  base: '/',
   plugins: [react(), tailwindcss()],
 })
